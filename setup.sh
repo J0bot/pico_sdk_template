f5226bbcca8253a7bcae6b9cac9c6e4796e0c520
Jobot <65437977+J0bot@users.noreply.github.com>
2021-11-16 15:26:50 +0100

    revome the build dir

--- setup.sh
+++ setup.sh
@@ -25,6 +25,7 @@ read -p "Already compile the projet ?" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+    rm -f -r -d build
     mkdir build
     cd build 
     cmake ..
