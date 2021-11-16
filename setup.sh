d21506f3777b52f7781b98179c6be75c182d3a9c
Jobot <65437977+J0bot@users.noreply.github.com>
2021-11-16 14:40:40 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+sudo apt install cmake gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib
+cd ~
+git clone https://github.com/raspberrypi/pico-sdk/
+cd pico-sdk
+git submodule update --init
+cd ~
+echo "export PICO_SDK_PATH=~/pico-sdk" >> '.bashrc'
+source .bashrc
+
+#creating the project
+mkdir project_template
+cd project_template
+
+#setup of the CmakeLists.txt file
+wget https://raw.githubusercontent.com/J0bot/pico_tests/files/CMakeLists.txt
