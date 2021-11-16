48ee7cb3ae4b5eec3ab6fd6829218a8db2ae1253
Jobot <65437977+J0bot@users.noreply.github.com>
2021-11-16 15:30:32 +0100

    adding of confirmations

--- setup.sh
+++ setup.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
+read -p "Reset previous installation ?" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    cd ~
+    rm -f -r -d project_template
+fi
 sudo apt install cmake gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib
 cd ~
 git clone https://github.com/raspberrypi/pico-sdk/
 cd pico-sdk
 git submodule update --init
 cd ~
-echo "export PICO_SDK_PATH=~/pico-sdk" >> '.bashrc'
+read -p "Already added pico-sdk to path ?" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "export PICO_SDK_PATH=~/pico-sdk" >> '.bashrc'
 source .bashrc
+fi
+
 
 #creating the project
 mkdir project_template
